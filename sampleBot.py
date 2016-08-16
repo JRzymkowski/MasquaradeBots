5aa612e98a997a066f81090acd70f294e256c981
JRzymkowski <j.rzymkowski@gmail.com>
2016-08-17 00:05:39 +0200

    Update sampleBot.py

--- sampleBot.py
+++ sampleBot.py
@@ -1,2 +1,12 @@
 # sample bot
 # not so simple bot
+
+import superposition.py
+
+class sampleBot:
+  board = None
+  fullySupportedCardSet = ['Judge', 'King', 'Queen', 'Cheat', 'Bishop', 'Spy', 'Thief']
+  supportedCardSet = ['Judge', 'King', 'Queen', 'Cheat', 'Bishop', 'Spy', 'Thief', 'Peasant', 'Witch', 'Widow', 'Inquisitor']
+  
+  def updateBelief():
+    
